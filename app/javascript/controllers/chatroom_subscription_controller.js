c07d770020d937c2b0d04169999837e55bdabd09
Adrien CARRION <adriencarrion006@gmail.com>
2022-06-17 15:18:31 +0200

    chatromm et name

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -18,9 +18,11 @@ export default class extends Controller {
 
 
   #insertMessageAndScrollDown(data) {
-  
+
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
   }
-
+  resetForm(event) {
+    event.target.reset()
+  }
 }
