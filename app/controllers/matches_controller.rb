75b5293b6049d978f02c3548b022c195d03cb264
Adrien CARRION <adriencarrion006@gmail.com>
2022-06-22 10:12:29 +0200

    swipe et photo

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,21 +1,27 @@
 class MatchesController < ApplicationController
-  def create
-  
-     match = (params[:match] == "true")
-    @match = Match.new
-    @match.user_id = current_user.id
-    @match.dog_id = params[:dog_id]
-    @match.match = match
-    if @match.save
-      if match
-        redirect_to dog_path(params[:dog_id])
-      else
+  # def create
 
-        redirect_to dogs_path
-      end
-    else
-    redirect_to dogs_path
-   end
+  #    match = (params[:match] == "true")
+  #   @match = Match.new
+  #   @match.user_id = current_user.id
+  #   @match.dog_id = params[:dog_id]
+  #   @match.match = match
+  #   if @match.save
+  #     if match
+  #       redirect_to dog_path(params[:dog_id])
+  #     else
 
+  #       redirect_to dogs_path
+  #     end
+  #   else
+  #   redirect_to dogs_path
+  #  end
+
+  # end
+  def new
+    @dog = Dog.find(params[:id])
+    params[:like] === "true" ? like = true : like = false
+    @dog.update(like: like)
+    redirect_to 
   end
 end
