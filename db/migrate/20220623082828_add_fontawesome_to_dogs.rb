283239a37c0c283ceba038c1c5370ea957d291e8
nordin Faraoun <nordin.faraoun@gmail.com>
2022-06-23 14:55:34 +0200

    icone2ter

---
+++ db/migrate/20220623082828_add_fontawesome_to_dogs.rb
@@ -0,0 +1,5 @@
+class AddFontawesomeToDogs < ActiveRecord::Migration[6.1]
+  def change
+    add_column :dogs, :fontawesome, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_22_080125) do
+ActiveRecord::Schema.define(version: 2022_06_23_082828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(version: 2022_06_22_080125) do
     t.text "picture3"
     t.boolean "like"
     t.string "centre_interet"
+    t.string "fontawesome"
     t.index ["user_id"], name: "index_dogs_on_user_id"
   end
 
