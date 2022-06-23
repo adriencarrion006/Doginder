fdf3c1ae24ffea9d0088219b8d3cbf09a2db7834
damien desparain <damien.cpielerins@gmail.com>
2022-06-23 12:27:51 +0200

    front show dog

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-         has_many :dogs
-         has_many_attached :photo
+        :recoverable, :rememberable, :validatable
+        has_many :dogs
+        has_many_attached :photo
 end
